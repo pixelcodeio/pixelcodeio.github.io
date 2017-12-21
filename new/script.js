a7a75a1f2e2b77fb2218dfd3a97e0cfdd74aaf79
Young Kim <young-k@protonmail.com>
2017-12-20 23:40:24 -0500

    increased duration of animation

--- new/script.js
+++ new/script.js
@@ -17,9 +17,9 @@ window.onload = function() {
     var elementTarget = document.getElementById("section-2");
     var max1 = 3.0;
     var max2 = 6.0;
-    if (window.scrollY < 200) {
-      max1 = 3.0 * window.scrollY / 200;
-      max2 = 6.0 * window.scrollY / 200;
+    if (window.scrollY < 400) {
+      max1 = 3.0 * window.scrollY / 400;
+      max2 = 6.0 * window.scrollY / 400;
       max1 = max1.toString();
       max2 = max2.toString();
       navbar.style.boxShadow = "0 "+ max1 +"px "+ max2 +"px 0 #dbdbdb";
