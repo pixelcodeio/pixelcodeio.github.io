dc5b97037af448c21af18baf84352a7b85cc4ec9
Young Kim <young-k@protonmail.com>
2017-12-20 22:41:01 -0500

    animations working

--- new/script.js
+++ new/script.js
@@ -1,4 +1,18 @@
 window.onload = function() {
+  var navbar = document.getElementById("navbar");
+  var elementTarget = document.getElementById("section-2");
+  var max1 = 3.0;
+  var max2 = 6.0;
+  console.log(window.scrollY);
+  if (window.scrollY < 200) {
+    max1 = 3.0 * window.scrollY / 200;
+    max2 = 6.0 * window.scrollY / 200;
+    max1 = max1.toString();
+    max2 = max2.toString();
+    navbar.style.boxShadow = "0 "+ max1 +"px "+ max2 +"px 0 #dbdbdb";
+  } else {
+    navbar.style.boxShadow = "0 3px 6px 0 #dbdbdb";
+  }
   window.addEventListener("scroll", function() {
     var navbar = document.getElementById("navbar");
     var elementTarget = document.getElementById("section-2");
@@ -11,7 +25,13 @@ window.onload = function() {
       max2 = max2.toString();
       navbar.style.boxShadow = "0 "+ max1 +"px "+ max2 +"px 0 #dbdbdb";
     } else {
-      navbar.style.boxShadow = "0 3 6 0 #dbdbdb";
+      navbar.style.boxShadow = "0 3px 6px 0 #dbdbdb";
     }
   });
+}
+
+function go_to(id) {
+  $('html,body').animate({
+    scrollTop: $(id).offset().top},
+    'slow');
 }
